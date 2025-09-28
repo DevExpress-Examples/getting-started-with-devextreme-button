584163781b3d07fc634efbdb1ba25cf02bfb3fd8
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-09-28 13:02:39 +0300

    Update to 25.1.3+

---
+++ Angular/src/app/orig_app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import notify from 'devextreme/ui/notify';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showMessage = () => {
+    notify("The button was clicked");
+  }
+}
---
+++ Vue/src/orig_App.vue
@@ -0,0 +1,37 @@
+<template>
+  <DxButton
+    text="Click me!"
+    @click="showMessage()"
+    styling-mode="outlined"
+    type="success"
+    icon="comment"
+  />
+</template>
+
+<script>
+import DxButton from 'devextreme-vue/button';
+import notify from 'devextreme/ui/notify';
+
+export default {
+  name: 'App',
+  components: {
+    DxButton
+  },
+  methods: {
+    showMessage() {
+      notify("The button was clicked");
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
